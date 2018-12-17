ba05a7a975cd5a8f36add8c4fb8a5f6c2906b619
Dmytro Pushkarchuk <pushkarchuk1986@gmai.com>
2018-12-18 01:56:35 +0200

    queue and deque

---
+++ JCF/collection/queue/app1.java
@@ -0,0 +1,52 @@
+package collection.queue;
+
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+@SuppressWarnings("all")
+
+public class app1 {
+
+	public static void main(String[] args) {
+
+//		Queue<Integer> queue = new ArrayBlockingQueue<Integer>(5);
+//		queue.add(1);
+//		queue.add(1);
+//		queue.add(1);
+//		queue.add(1);
+//		System.out.println(queue.offer(1));
+//		System.out.println(queue.offer(1));
+//		System.out.println(queue);
+		
+
+//		Deque<Integer> queue = new ArrayDeque<Integer>(5);
+//		queue.add(1);
+//		queue.add(2);
+//		queue.add(3);
+//		queue.add(4);
+//		queue.add(5);
+//		System.out.println(queue);
+//		System.out.println("element : " + queue.element());
+//		System.out.println("remove : " + queue.remove());
+//		System.out.println(queue);
+		
+//		Queue <Integer> asLifoQueue = Collections.asLifoQueue(queue);
+//		
+//		
+//		asLifoQueue.add(6);
+//		asLifoQueue.add(7);
+//		asLifoQueue.add(8);
+//		asLifoQueue.add(9);
+//		asLifoQueue.add(10);
+//		asLifoQueue.add(11);
+//		
+//		System.out.println(asLifoQueue);
+
+	
+	
+	
+	}
+}
