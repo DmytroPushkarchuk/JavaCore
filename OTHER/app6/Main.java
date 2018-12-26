d3d1d137f9a719a476ee662e30c0bd9687783f3a
Dmytro Pushkarchuk <pushkarchuk1986@gmai.com>
2018-12-26 16:01:46 +0200

    app6

---
+++ OTHER/app6/Main.java
@@ -0,0 +1,20 @@
+package app6;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String a = new String ("d");
+		String b = a;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		b = "c";
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+
+}
